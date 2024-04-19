8bc77efd25622c15bc4ac80b95c48dbfbfba9f1f
Your Name <tiagomixpt@gmail.com>
2024-04-19 16:31:42 +0100

    Atividade3

---
+++ CompAerea.Dominio/Entidades/Avioes.cs
@@ -0,0 +1,15 @@
+﻿namespace CompAerea.Dominio.Entidades
+{
+    public class Avioes
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public string? Descricao { get; set; }
+        public double Preco { get; set; }
+        public int Classe { get; set; }
+        public int Passageiros { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? DataPartida { get; set; }
+        public DateTime? DataChegada { get; set; }
+    }
+}
---
+++ CompAerea.Infrastrutura/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CompAerea.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompAerea.Infrastrutura.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+            
+        }
+
+        public DbSet<Avioes> Avioes { get; set; }
+    }
+}
